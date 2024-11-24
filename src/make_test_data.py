d9f4143031601c1f7619113a36d4630f8ade7762
Marshall Fisher <mfisher@Marshalls-Air.attlocal.net>
2024-11-24 15:24:37 -0800

    update scripts

--- src/make_test_data.py
+++ src/make_test_data.py
@@ -6,9 +6,32 @@ import sys
 # Set seed for reproducibility
 random.seed(414)
 
-def main(data_dir, rm):
+def make_test_data(data_dir, rm):
     all_files = os.listdir(data_dir)
     test_data_name = 'mpd.slice.{x}00000-{x}00999.json'
+    test_files = [test_data_name.format(x=i) for i in range(1, 10)]
+    test_files.append('mpd.slice.0-999.json')
+    assert set(test_files).issubset(all_files)
+    test_json = {'info': [], 'playlists': []}
+    for f in test_files:
+        with open(os.path.join(data_dir, f)) as fh:
+            curr_json = json.load(fh)
+            test_json['info'].append(curr_json['info'])
+            test_json['playlists'].extend(curr_json['playlists'])
+    # shuffle playlists for good measure
+    # note that this does not actually shuffle the order
+    # of each playlist's tracks, which is important to preserve.
+    random.shuffle(test_json['playlists'])
+    os.makedirs('test_data', exist_ok=True)
+    with open(os.path.join('test_data', 'mpd.test.json'), "w+") as fh:
+        json.dump(test_json, fh, indent=4)
+    if rm:
+        for f in test_files:
+            os.remove(f)
+
+def make_validation_data(data_dir, rm):
+    all_files = os.listdir(data_dir)
+    validation_data_prefix = 'mpd.slice.9'
     test_files = [test_data_name.format(x=i) for i in range(1, 10)]
     test_files.append('mpd.slice.0-999.json')
     assert set(test_files).issubset(all_files)
@@ -32,8 +55,14 @@ def main(data_dir, rm):
 
 if __name__ == "__main__":
     data_dir = sys.argv[1]
+    mode = sys.argv[2]
     rm = False
-    if len(sys.argv) > 2 and sys.argv[2] == "--rm":
+    if len(sys.argv) > 3 and sys.argv[3] == "--rm":
         rm = True
-    main(data_dir, rm)
+    if mode == "test":
+        make_test_data(data_dir, rm)
+    elif mode == "validation":
+        make_validation_data(data_dir, rm)
+    else:
+        raise ValueError(f"Invalid value provided for mode: {mode}")
 
--- src/split_test_data.py
+++ src/split_test_data.py
@@ -6,30 +6,40 @@ import sys
 random.seed(414)
 
 
-def _make_data_slice(playlist_set, fetcher, data_name, test_data_dir):
+def _make_data_slice(playlist_set, fetcher, data_name, output_data_dir):
     curr_json = []
     for playlist in playlist_set:
         curr_json.append(fetcher(playlist))
-    with open(os.path.join(test_data_dir, f"{data_name}.json"), "w+") as fh:
+    with open(os.path.join(output_data_dir, f"{data_name}.json"), "w+") as fh:
         json.dump(curr_json, fh, indent=4)
     with open(
-        os.path.join(test_data_dir, f"ground_truth_{data_name}.json"), "w+"
+        os.path.join(output_data_dir, f"ground_truth_{data_name}.json"), "w+"
     ) as fh:
         json.dump(playlist_set, fh, indent=4)
 
 
+def make_generator(lst, chunk_size):
+    # c/o gpt4
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i : i + chunk_size]
+
+
 def main(data_file):
-    test_data_dir = os.path.dirname(data_file)
+    output_data_dir = os.path.dirname(data_file)
     with open(data_file) as fh:
         test_json = json.load(fh)
-    hundred_track_playlists = [
-        x for x in test_json["playlists"] if len(x["tracks"]) > 100
-    ]
-    random.shuffle(hundred_track_playlists)
-    short_playlists = [x for x in test_json["playlists"] if len(x["tracks"]) <= 100]
-    random.shuffle(short_playlists)
 
-    title_and_100_tracks = hundred_track_playlists[:1000]
+    # NOTE: this sorting (or a similar approach) is necessary to ensure
+    # that e.g. 100 and 25 track test playlists actually have enough tracks,
+    # but it may introduce statistical biases into the test set.
+    # TODO (mfisher): Implement a generator approach that shuffles the order
+    # and then returns playlists with length >= min length for each split
+    playlists = [x for x in test_json["playlists"]]
+    playlists = sorted(playlists, key=lambda x: len(x["tracks"]), reverse=True)
+    playlists = make_generator(playlists, chunk_size=1000)
+
+    title_and_100_tracks = next(playlists)
+    assert all(len(x["tracks"]) > 100 for x in title_and_100_tracks)
     fetch_title_and_100_tracks = lambda playlist: {
         "name": playlist["name"],
         "tracks": playlist["tracks"][:100],
@@ -38,10 +48,11 @@ def main(data_file):
         title_and_100_tracks,
         fetch_title_and_100_tracks,
         "title_and_first_100_tracks",
-        test_data_dir,
+        output_data_dir,
     )
 
-    title_and_100_random_tracks = hundred_track_playlists[1000:2000]
+    title_and_100_random_tracks = next(playlists)
+    assert all(len(x["tracks"]) > 100 for x in title_and_100_random_tracks)
     fetch_title_and_100_random_tracks = lambda playlist: {
         "name": playlist["name"],
         "tracks": random.sample(playlist["tracks"], 100),
@@ -50,44 +61,24 @@ def main(data_file):
         title_and_100_random_tracks,
         fetch_title_and_100_random_tracks,
         "title_and_random_100_tracks",
-        test_data_dir,
+        output_data_dir,
     )
 
-    playlists = short_playlists + hundred_track_playlists[2000:]
-    random.shuffle(playlists)
-
-    title_only = lambda playlist: {"name": playlist["name"]}
-    _make_data_slice(playlists[:1000], title_only, "title_only", test_data_dir)
-
-    title_and_first_five = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": playlist["tracks"][:5],
-    }
-    _make_data_slice(playlists[1000:2000], title_and_first_five, "title_and_first_5_tracks", test_data_dir)
-
-    first_five = lambda playlist: {
-        "tracks": playlist["tracks"][:5],
-    }
-    _make_data_slice(playlists[2000:3000], first_five, "first_5_tracks", test_data_dir)
-
-    title_and_first_ten = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": playlist["tracks"][:10],
-    }
-    _make_data_slice(playlists[3000:4000], title_and_first_ten, "title_and_first_10_tracks", test_data_dir)
-
-    first_ten = lambda playlist: {
-        "tracks": playlist["tracks"][:10],
-    }
-    _make_data_slice(playlists[4000:5000], first_ten, "first_10_tracks", test_data_dir)
-
-    title_and_first_twenty_five = lambda playlist: {
+    title_and_first_25 = next(playlists)
+    fetch_title_and_first_twenty_five = lambda playlist: {
         "name": playlist["name"],
         "tracks": playlist["tracks"][:25],
     }
-    _make_data_slice(playlists[5000:6000], title_and_first_twenty_five, "title_and_first_25_tracks", test_data_dir)
+    assert all(len(x["tracks"]) > 25 for x in title_and_first_25)
+    _make_data_slice(
+        title_and_first_25,
+        fetch_title_and_first_twenty_five,
+        "title_and_first_25_tracks",
+        output_data_dir,
+    )
 
-    title_and_25_random_tracks = hundred_track_playlists[6000:7000]
+    title_and_25_random_tracks = next(playlists)
+    assert all(len(x["tracks"]) > 25 for x in title_and_25_random_tracks)
     fetch_title_and_25_random_tracks = lambda playlist: {
         "name": playlist["name"],
         "tracks": random.sample(playlist["tracks"], 25),
@@ -96,20 +87,67 @@ def main(data_file):
         title_and_25_random_tracks,
         fetch_title_and_25_random_tracks,
         "title_and_random_25_tracks",
-        test_data_dir,
+        output_data_dir,
     )
 
-    title_and_first_track = hundred_track_playlists[7000:8000]
+    title_and_first_ten = next(playlists)
+    assert all(len(x["tracks"]) > 10 for x in title_and_first_ten)
+    fetch_title_and_first_ten = lambda playlist: {
+        "name": playlist["name"],
+        "tracks": playlist["tracks"][:10],
+    }
+    _make_data_slice(
+        title_and_first_ten,
+        fetch_title_and_first_ten,
+        "title_and_first_10_tracks",
+        output_data_dir,
+    )
+
+    first_ten = next(playlists)
+    assert all(len(x["tracks"]) > 10 for x in title_and_first_ten)
+    fetch_first_ten = lambda playlist: {
+        "tracks": playlist["tracks"][:10],
+    }
+    _make_data_slice(first_ten, fetch_first_ten, "first_10_tracks", output_data_dir)
+
+    title_and_first_five = next(playlists)
+    assert all(len(x["tracks"]) > 5 for x in title_and_first_ten)
+    fetch_title_and_first_five = lambda playlist: {
+        "name": playlist["name"],
+        "tracks": playlist["tracks"][:5],
+    }
+    _make_data_slice(
+        title_and_first_five,
+        fetch_title_and_first_five,
+        "title_and_first_5_tracks",
+        output_data_dir,
+    )
+
+    first_five = next(playlists)
+    assert all(len(x["tracks"]) > 5 for x in first_five)
+    fetch_first_five = lambda playlist: {
+        "tracks": playlist["tracks"][:5],
+    }
+    _make_data_slice(first_five, fetch_first_five, "first_5_tracks", output_data_dir)
+
+    title_and_first_track = next(playlists)
+    assert all(len(x["tracks"]) > 1 for x in title_and_first_track)
     fetch_title_and_first_track = lambda playlist: {
         "name": playlist["name"],
-        "tracks": playlist["tracks"][0]
+        "tracks": playlist["tracks"][0],
     }
     _make_data_slice(
         title_and_first_track,
         fetch_title_and_first_track,
         "title_and_first_1_track",
-        test_data_dir,
+        output_data_dir,
     )
+
+    title_only = next(playlists)
+    assert all(len(x["tracks"]) > 1 for x in title_only)
+    fetch_title_only = lambda playlist: {"name": playlist["name"]}
+    _make_data_slice(title_only, fetch_title_only, "title_only", output_data_dir)
+
 
 if __name__ == "__main__":
     data_file = sys.argv[1]
