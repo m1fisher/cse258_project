c3f1118df01f6309c6aece74aed518a350d7dcb1
Marshall Fisher <m1fisher@ucsd.edu>
2024-11-25 19:50:40 -0800

    update split_test_data.py to use csv

--- src/split_test_data.py
+++ src/split_test_data.py
@@ -1,21 +1,18 @@
-import json
+from collections import defaultdict
+import csv
+from itertools import chain
 import os
 import random
 import sys
 
+from utils import Track, write_track_csv
+
 random.seed(414)
 
 
-def _make_data_slice(playlist_set, fetcher, data_name, output_data_dir):
-    curr_json = []
-    for playlist in playlist_set:
-        curr_json.append(fetcher(playlist))
-    with open(os.path.join(output_data_dir, f"{data_name}.json"), "w+") as fh:
-        json.dump(curr_json, fh, indent=4)
-    with open(
-        os.path.join(output_data_dir, f"ground_truth_{data_name}.json"), "w+"
-    ) as fh:
-        json.dump(playlist_set, fh, indent=4)
+def _make_data_slice(ground_truth, test_set, data_name, output_data_dir):
+    write_track_csv(test_set, os.path.join(output_data_dir, f"{data_name}.csv"))
+    write_track_csv(ground_truth, os.path.join(output_data_dir, f"ground_truth_{data_name}.csv"))
 
 
 def make_generator(lst, chunk_size):
@@ -23,130 +20,144 @@ def make_generator(lst, chunk_size):
     for i in range(0, len(lst), chunk_size):
         yield lst[i : i + chunk_size]
 
+def flatten(nested_list):
+    return list(chain.from_iterable(nested_list))
+
 
 def main(data_file):
     output_data_dir = os.path.dirname(data_file)
     with open(data_file) as fh:
-        test_json = json.load(fh)
+       reader = csv.reader(fh)
+       data = []
+       for row in reader:
+           if 'pid' not in row:  # disregard header
+               data.append(Track(*(int(x) for x in row)))
+    playlist_map = defaultdict(list)
+    for track in data:
+        playlist_map[track.pid].append(track)
+    playlists = [p for p in playlist_map.values()]
 
     # NOTE: this sorting (or a similar approach) is necessary to ensure
-    # that e.g. 100 and 25 track test playlists actually have enough tracks,
-    # but it may introduce statistical biases into the test set.
-    # TODO (mfisher): Implement a generator approach that shuffles the order
-    # and then returns playlists with length >= min length for each split
-    playlists = [x for x in test_json["playlists"]]
-    playlists = sorted(playlists, key=lambda x: len(x["tracks"]), reverse=True)
+    # that e.g. 100 and 25 track test playlists actually have enough tracks.
+    playlists = sorted(playlists, key=len, reverse=True)
     playlists = make_generator(playlists, chunk_size=1000)
 
-    title_and_100_tracks = next(playlists)
-    assert all(len(x["tracks"]) > 100 for x in title_and_100_tracks)
-    fetch_title_and_100_tracks = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": playlist["tracks"][:100],
-    }
+    title_and_first_100_tracks = next(playlists)
+    assert all(len(x) > 100 for x in title_and_first_100_tracks)
+    test = flatten([x[:100] for x in title_and_first_100_tracks])
+    ground_truth = flatten([x[100:] for x in title_and_first_100_tracks])
     _make_data_slice(
-        title_and_100_tracks,
-        fetch_title_and_100_tracks,
+        ground_truth,
+        test,
         "title_and_first_100_tracks",
         output_data_dir,
     )
 
     title_and_100_random_tracks = next(playlists)
-    assert all(len(x["tracks"]) > 100 for x in title_and_100_random_tracks)
-    fetch_title_and_100_random_tracks = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": random.sample(playlist["tracks"], 100),
-    }
+    for p in title_and_100_random_tracks:
+        random.shuffle(p)
+    assert all(len(x) > 100 for x in title_and_100_random_tracks)
+    test = flatten([x[:100] for x in title_and_100_random_tracks])
+    ground_truth = flatten([x[100:] for x in title_and_100_random_tracks])
     _make_data_slice(
-        title_and_100_random_tracks,
-        fetch_title_and_100_random_tracks,
+        ground_truth,
+        test,
         "title_and_random_100_tracks",
         output_data_dir,
     )
 
-    title_and_first_25 = next(playlists)
-    fetch_title_and_first_twenty_five = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": playlist["tracks"][:25],
-    }
-    assert all(len(x["tracks"]) > 25 for x in title_and_first_25)
+    title_and_first_25_tracks = next(playlists)
+    assert all(len(x) > 25 for x in title_and_first_25_tracks)
+    test = flatten([x[:25] for x in title_and_first_25_tracks])
+    ground_truth = flatten([x[25:] for x in title_and_first_25_tracks])
     _make_data_slice(
-        title_and_first_25,
-        fetch_title_and_first_twenty_five,
+        ground_truth,
+        test,
         "title_and_first_25_tracks",
         output_data_dir,
     )
 
     title_and_25_random_tracks = next(playlists)
-    assert all(len(x["tracks"]) > 25 for x in title_and_25_random_tracks)
-    fetch_title_and_25_random_tracks = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": random.sample(playlist["tracks"], 25),
-    }
+    for p in title_and_25_random_tracks:
+        random.shuffle(p)
+    assert all(len(x) > 25 for x in title_and_25_random_tracks)
+    test = flatten([x[:25] for x in title_and_25_random_tracks])
+    ground_truth = flatten([x[25:] for x in title_and_25_random_tracks])
     _make_data_slice(
-        title_and_25_random_tracks,
-        fetch_title_and_25_random_tracks,
+        ground_truth,
+        test,
         "title_and_random_25_tracks",
         output_data_dir,
     )
 
-    title_and_first_ten = next(playlists)
-    assert all(len(x["tracks"]) > 10 for x in title_and_first_ten)
-    fetch_title_and_first_ten = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": playlist["tracks"][:10],
-    }
+    title_and_first_10_tracks = next(playlists)
+    assert all(len(x) > 10 for x in title_and_first_10_tracks)
+    test = flatten([x[:10] for x in title_and_first_10_tracks])
+    ground_truth = flatten([x[10:] for x in title_and_first_10_tracks])
     _make_data_slice(
-        title_and_first_ten,
-        fetch_title_and_first_ten,
+        ground_truth,
+        test,
         "title_and_first_10_tracks",
         output_data_dir,
     )
 
     first_ten = next(playlists)
-    assert all(len(x["tracks"]) > 10 for x in title_and_first_ten)
-    fetch_first_ten = lambda playlist: {
-        "tracks": playlist["tracks"][:10],
-    }
-    _make_data_slice(first_ten, fetch_first_ten, "first_10_tracks", output_data_dir)
-
-    title_and_first_five = next(playlists)
-    assert all(len(x["tracks"]) > 5 for x in title_and_first_ten)
-    fetch_title_and_first_five = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": playlist["tracks"][:5],
-    }
+    assert all(len(x) > 10 for x in first_ten)
+    test = flatten([x[:10] for x in first_ten])
+    ground_truth = flatten([x[10:] for x in first_ten])
+    for x in test:
+        # Scrub the playlist ID
+        x.pid = None
     _make_data_slice(
-        title_and_first_five,
-        fetch_title_and_first_five,
+        ground_truth,
+        test,
+        "first_10_tracks",
+        output_data_dir,
+    )
+
+    title_and_first_5_tracks = next(playlists)
+    assert all(len(x) > 5 for x in title_and_first_5_tracks)
+    test = flatten([x[:5] for x in title_and_first_5_tracks])
+    ground_truth = flatten([x[5:] for x in title_and_first_5_tracks])
+    _make_data_slice(
+        ground_truth,
+        test,
         "title_and_first_5_tracks",
         output_data_dir,
     )
 
-    first_five = next(playlists)
-    assert all(len(x["tracks"]) > 5 for x in first_five)
-    fetch_first_five = lambda playlist: {
-        "tracks": playlist["tracks"][:5],
-    }
-    _make_data_slice(first_five, fetch_first_five, "first_5_tracks", output_data_dir)
-
-    title_and_first_track = next(playlists)
-    assert all(len(x["tracks"]) > 1 for x in title_and_first_track)
-    fetch_title_and_first_track = lambda playlist: {
-        "name": playlist["name"],
-        "tracks": playlist["tracks"][0],
-    }
+    first_5 = next(playlists)
+    assert all(len(x) > 5 for x in first_5)
+    test = flatten([x[:5] for x in first_5])
+    ground_truth = flatten([x[5:] for x in first_5])
+    for x in test:
+        # Scrub the playlist ID
+        x.pid = None
     _make_data_slice(
-        title_and_first_track,
-        fetch_title_and_first_track,
-        "title_and_first_1_track",
+        ground_truth,
+        test,
+        "first_5_tracks",
+        output_data_dir,
+    )
+
+
+    title_and_first_1_tracks = next(playlists)
+    assert all(len(x) > 1 for x in title_and_first_1_tracks)
+    test = flatten([x[:1] for x in title_and_first_1_tracks])
+    ground_truth = flatten([x[1:] for x in title_and_first_1_tracks])
+    _make_data_slice(
+        ground_truth,
+        test,
+        "title_and_first_1_tracks",
         output_data_dir,
     )
 
     title_only = next(playlists)
-    assert all(len(x["tracks"]) > 1 for x in title_only)
-    fetch_title_only = lambda playlist: {"name": playlist["name"]}
-    _make_data_slice(title_only, fetch_title_only, "title_only", output_data_dir)
+    assert all(len(x) > 1 for x in title_only)
+    title_only = flatten(title_only)
+    pid_set = set(x.pid for x in title_only)
+    pids = [Track(pid, None, None, None, None) for pid in pid_set]
+    _make_data_slice(pids, title_only, "title_only", output_data_dir)
 
 
 if __name__ == "__main__":
