4d5fe4d7ef8a9426d2fa81ba2ab9440d074ab05c
Marshall Fisher <mfisher@Marshalls-Air.attlocal.net>
2024-11-25 19:10:20 -0800

    update make_test_data to run for csv files

--- src/make_test_data.py
+++ src/make_test_data.py
@@ -1,4 +1,4 @@
-import json
+import csv
 import os
 import random
 import shutil
@@ -8,99 +8,111 @@ import sys
 random.seed(414)
 
 VALIDATION_FILES = [
-    "mpd.slice.954000-954999.json",
-    "mpd.slice.955000-955999.json",
-    "mpd.slice.956000-956999.json",
-    "mpd.slice.957000-957999.json",
-    "mpd.slice.958000-958999.json",
-    "mpd.slice.959000-959999.json",
-    "mpd.slice.96000-96999.json",
-    "mpd.slice.960000-960999.json",
-    "mpd.slice.961000-961999.json",
-    "mpd.slice.962000-962999.json",
-    "mpd.slice.963000-963999.json",
-    "mpd.slice.964000-964999.json",
-    "mpd.slice.965000-965999.json",
-    "mpd.slice.966000-966999.json",
-    "mpd.slice.967000-967999.json",
-    "mpd.slice.968000-968999.json",
-    "mpd.slice.969000-969999.json",
-    "mpd.slice.97000-97999.json",
-    "mpd.slice.970000-970999.json",
-    "mpd.slice.971000-971999.json",
-    "mpd.slice.972000-972999.json",
-    "mpd.slice.973000-973999.json",
-    "mpd.slice.974000-974999.json",
-    "mpd.slice.975000-975999.json",
-    "mpd.slice.976000-976999.json",
-    "mpd.slice.977000-977999.json",
-    "mpd.slice.978000-978999.json",
-    "mpd.slice.979000-979999.json",
-    "mpd.slice.98000-98999.json",
-    "mpd.slice.980000-980999.json",
-    "mpd.slice.981000-981999.json",
-    "mpd.slice.982000-982999.json",
-    "mpd.slice.983000-983999.json",
-    "mpd.slice.984000-984999.json",
-    "mpd.slice.985000-985999.json",
-    "mpd.slice.986000-986999.json",
-    "mpd.slice.987000-987999.json",
-    "mpd.slice.988000-988999.json",
-    "mpd.slice.989000-989999.json",
-    "mpd.slice.99000-99999.json",
-    "mpd.slice.990000-990999.json",
-    "mpd.slice.991000-991999.json",
-    "mpd.slice.992000-992999.json",
-    "mpd.slice.993000-993999.json",
-    "mpd.slice.994000-994999.json",
-    "mpd.slice.995000-995999.json",
-    "mpd.slice.996000-996999.json",
-    "mpd.slice.997000-997999.json",
-    "mpd.slice.998000-998999.json",
-    "mpd.slice.999000-999999.json",
+    "mpd.slice.954000-954999.csv",
+    "mpd.slice.955000-955999.csv",
+    "mpd.slice.956000-956999.csv",
+    "mpd.slice.957000-957999.csv",
+    "mpd.slice.958000-958999.csv",
+    "mpd.slice.959000-959999.csv",
+    "mpd.slice.96000-96999.csv",
+    "mpd.slice.960000-960999.csv",
+    "mpd.slice.961000-961999.csv",
+    "mpd.slice.962000-962999.csv",
+    "mpd.slice.963000-963999.csv",
+    "mpd.slice.964000-964999.csv",
+    "mpd.slice.965000-965999.csv",
+    "mpd.slice.966000-966999.csv",
+    "mpd.slice.967000-967999.csv",
+    "mpd.slice.968000-968999.csv",
+    "mpd.slice.969000-969999.csv",
+    "mpd.slice.97000-97999.csv",
+    "mpd.slice.970000-970999.csv",
+    "mpd.slice.971000-971999.csv",
+    "mpd.slice.972000-972999.csv",
+    "mpd.slice.973000-973999.csv",
+    "mpd.slice.974000-974999.csv",
+    "mpd.slice.975000-975999.csv",
+    "mpd.slice.976000-976999.csv",
+    "mpd.slice.977000-977999.csv",
+    "mpd.slice.978000-978999.csv",
+    "mpd.slice.979000-979999.csv",
+    "mpd.slice.98000-98999.csv",
+    "mpd.slice.980000-980999.csv",
+    "mpd.slice.981000-981999.csv",
+    "mpd.slice.982000-982999.csv",
+    "mpd.slice.983000-983999.csv",
+    "mpd.slice.984000-984999.csv",
+    "mpd.slice.985000-985999.csv",
+    "mpd.slice.986000-986999.csv",
+    "mpd.slice.987000-987999.csv",
+    "mpd.slice.988000-988999.csv",
+    "mpd.slice.989000-989999.csv",
+    "mpd.slice.99000-99999.csv",
+    "mpd.slice.990000-990999.csv",
+    "mpd.slice.991000-991999.csv",
+    "mpd.slice.992000-992999.csv",
+    "mpd.slice.993000-993999.csv",
+    "mpd.slice.994000-994999.csv",
+    "mpd.slice.995000-995999.csv",
+    "mpd.slice.996000-996999.csv",
+    "mpd.slice.997000-997999.csv",
+    "mpd.slice.998000-998999.csv",
+    "mpd.slice.999000-999999.csv",
 ]
 
 
 def make_test_data(data_dir, mv):
     all_files = os.listdir(data_dir)
-    test_data_name = "mpd.slice.{x}00000-{x}00999.json"
+    test_data_name = "mpd.slice.{x}00000-{x}00999.csv"
     test_files = [test_data_name.format(x=i) for i in range(1, 10)]
-    test_files.append("mpd.slice.0-999.json")
+    test_files.append("mpd.slice.0-999.csv")
     assert set(test_files).issubset(all_files)
-    test_json = {"info": [], "playlists": []}
+    test_csv = []
     for f in test_files:
         with open(os.path.join(data_dir, f)) as fh:
-            curr_json = json.load(fh)
-            test_json["info"].append(curr_json["info"])
-            test_json["playlists"].extend(curr_json["playlists"])
+            reader = csv.reader(fh)
+            curr_csv = []
+            for row in reader:
+                # Convert each row to a tuple and append to the list
+                if 'pid' not in row:  # disregard header
+                    curr_csv.append(tuple(row))
+            test_csv.append(curr_csv)
     out_dir = "test_data"
-    filename = "mpd.test.json"
-    make_output_file(test_json, test_files, out_dir, filename)
+    filename = "mpd.test.csv"
+    make_output_file(test_csv, test_files, out_dir, filename)
 
 
 def make_validation_data(data_dir, mv):
     all_files = sorted(os.listdir(data_dir))
     # Use the 50 files (50k playlists) for validation
     validation_files = VALIDATION_FILES
-    validation_json = {"info": [], "playlists": []}
+    validation_csv = []
     for f in validation_files:
         with open(os.path.join(data_dir, f)) as fh:
-            curr_json = json.load(fh)
-            validation_json["info"].append(curr_json["info"])
-            validation_json["playlists"].extend(curr_json["playlists"])
+            reader = csv.reader(fh)
+            curr_csv = []
+            for row in reader:
+                # Convert each row to a tuple and append to the list
+                if 'pid' not in row:  # disregard header
+                    curr_csv.append(tuple(row))
+            validation_csv.append(curr_csv)
     out_dir = "validation_data"
-    filename = "mpd.validation.json"
-    make_output_file(validation_json, validation_files, out_dir, filename)
+    filename = "mpd.validation.csv"
+    make_output_file(validation_csv, validation_files, out_dir, filename)
 
 
-def make_output_file(json_list, file_list, out_dir, filename):
+def make_output_file(csv_list, file_list, out_dir, filename):
     # shuffle playlists for good measure
     # note that this does not actually shuffle the order
     # of each playlist's tracks, which is important to preserve.
-    random.shuffle(json_list["playlists"])
+    random.shuffle(csv_list)
     os.makedirs(out_dir, exist_ok=True)
+    all_rows = [('pid', 'pos', 'track_id', 'artist_id', 'album_id')]
+    for one_csv in csv_list:
+        all_rows.extend(one_csv)
     with open(os.path.join(out_dir, filename), "w+") as fh:
-        json.dump(json_list, fh, indent=4)
+        writer = csv.writer(fh)
+        writer.writerows(all_rows)
     if mv:
         for f in file_list:
             shutil.move(os.path.join(data_dir, f), os.path.join(out_dir, f))
