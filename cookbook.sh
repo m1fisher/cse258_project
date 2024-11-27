6e1e598ddd1410b6415f4286d9a61c2a87353dce
Marshall Fisher <m1fisher@ucsd.edu>
2024-11-26 20:25:18 -0800

    add cookbook.sh

---
+++ cookbook.sh
@@ -0,0 +1,13 @@
+# First, unzip the archive such that the data is available in subdirectory `data/`
+# Process data to smaller CSVs
+#python3 src/process_data.py data processed_data
+# Create test data and move it out of the processed_data/ dir
+python3 src/make_test_data.py processed_data test --mv
+# Split the test data into different evaluation categories used in the Spotify challenge
+python3 src/split_test_data.py test_data/mpd.test.csv
+# Create validation data and move it out of the data/ dir
+python3 src/make_test_data.py processed_data validation --mv
+# Split the validation data into different evaluation categories used in the Spotify challenge
+python3 src/split_test_data.py validation_data/mpd.validation.csv
+# What remains in the `processed_data/` subdir may now be used as training data
+mv processed_data/ train_data/
